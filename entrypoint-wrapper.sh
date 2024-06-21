3f9b1d967a48c96f6d10c4cf489253990b724e2e
MRColor <mattiar11@gmail.com>
2024-06-21 14:23:31 +0200

    chg : reduced waiting time

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -7,8 +7,8 @@ if [ -f /app/entrypoint.sh ]; then
   echo "Running the original entry point in the background..."
   /app/entrypoint.sh &
   echo "Original entry point is running."
-  # Wait for 30 seconds
-  sleep 30
+  # Wait for 10 seconds
+  sleep 10
 fi
 
 # Run the Python script
